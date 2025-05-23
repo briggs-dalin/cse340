cf091315a6b1f05362d7b644c3a84c5ac747b108
= <briggs.dalin09@gmail.com>
2025-05-23 12:56:50 -0600

    Week 3 Finished

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 /* ******************************************
- * This server.js file is the primary file of the 
+ * This server.js file is the primary file of the
  * application. It is used to control the project.
  *******************************************/
 /* ***********************
@@ -10,8 +10,9 @@ const expressLayouts = require("express-ejs-layouts");
 const env = require("dotenv").config();
 const app = express();
 const static = require("./routes/static");
-const baseContoller = require("./controllers/baseController");
+const baseController = require("./controllers/baseController");
 const inventoryRoute = require("./routes/inventoryRoute.js");
+const intentionalErrorRoute = require("./routes/intentionalErrorRoute.js");
 const utilities = require("./utilities/index.js");
 
 /* ***********************
@@ -21,18 +22,39 @@ app.set("view engine", "ejs");
 app.use(expressLayouts);
 app.set("layout", "./layouts/layout"); // Not at view root
 
+
 /* ***********************
  * Routes
  *************************/
-app.set("view engine", "ejs");
-app.use(expressLayouts);
-app.set("layout", "./layouts/layout"); // not at views root
 app.use(static);
-//Index route
-app.get("/", baseContoller.buildHome)
+// Index route
+app.get("/", utilities.handleErrors(baseController.buildHome));
 // Inventory routes
-app.use("/inv", inventoryRoute)
+app.use("/inv", inventoryRoute);
+// Intentional error route. Used for testing
+app.use("/ierror", intentionalErrorRoute);
 
+// File Not Found Route - must be last route in list
+app.use(async (req, res, next) => {
+  next({status: 404, message: 'Unfortunately, we don\'t have that page in stock.'})
+})
+
+
+/* ***********************
+* Express Error Handler
+* Place after all other middleware
+*************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  console.dir(err);
+  if(err.status == 404){ message = err.message} else {message = 'Oh no! There was a crash. Maybe try a different route?'}
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message,
+    nav
+  })
+})
 
 
 /* ***********************
